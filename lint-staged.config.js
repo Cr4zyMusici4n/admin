dc3e501c4dd16937babd24c3d8c8901e35b549c1
Daniil <daniil18rusxxx@gmail.com>
2026-09-17 01:27:10 +0400

    :wrench: CONFIG(LintStaged): Fix tsc in lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
   "*.css": ["stylelint", "oxfmt"],
   "*.{ts,tsx,js,jsx,json,mjs,mts}": ["eslint"],
-  "*.{ts,tsx}": () => "tsc -b --noEmit",
+  "*.{ts,tsx}": () => "tsc --noEmit -p tsconfig.app.json",
 };
